fcfa181a69905e4d1b85f61d14c5061e4af156e1
Brandon A <br.araki@gmail.com>
2017-07-25 18:13:22 -0400

    particle filter more or less works now

--- src/measurement_filter.py
+++ src/measurement_filter.py
@@ -22,14 +22,18 @@ class ParticleFilter(object):
     def __init__(self):
         self.Np = rospy.get_param("~num_particles", 100)
         self.Ncars = rospy.get_param("~num_cars", 3)
-        self.Ndim = rospy.get_param("~num_state_dim", 2)
-        self.Nmeas = rospy.get_param("~num_measurements", 5)
+        self.Ndim = rospy.get_param("~num_state_dim", 5)
+        self.Nmeas = rospy.get_param("~num_measurements", 6)
 
-        self.x0 = np.array([[0, 0], [2, 1], [0, 1]], dtype=np.float64)
+        self.x0 = np.array([[0, 0, math.pi, 0, 0],
+                   [2, 1, math.pi/3., 0, 0],
+                   [0, 1, 0, 0, 0]],
+                  dtype=np.float64)
         
-        self.x_cov = np.diag(self.Ncars * self.Ndim * [0.01])
-        self.meas_cov = np.diag(self.Ncars * [0.1, 0.1, 0.01, 0.01, 0.01])
-        self.control_cov = np.diag(self.Ncars * self.Ndim * [0.1])
+        self.init_cov = np.diag(self.Ncars * [0.6, 0.6, 0.1, 0.001, 0.001])
+        self.x_cov = np.diag(self.Ncars * [0.1, 0.1, 0.1, 0.04, 0.00])
+        self.meas_cov = np.diag(self.Ncars * [0.6, 0.6, 0.1, 0.1, 0.1, 0.01])
+        #self.control_cov = np.diag(self.Ncars * self.Ndim * [0.1])
         self.resample_perc = rospy.get_param("~resample_perc", 0.3)
         self.particles = np.random.multivariate_normal(
             self.x0.flatten(),
@@ -39,11 +43,11 @@ class ParticleFilter(object):
 
         self.prev_time = rospy.get_time()
         self.current_time = 0
-        self.v_cov = np.diag(self.Ncars * self.Ndim * [0.1])
-        self.v_func = lambda t: np.array([
-            [0.1 * t, 3 * np.cos(t)],
-            [2 * np.cos(0.5 * t), 1 * np.sin(t)],
-            [1.4 * np.cos(t), 3 * np.sin(0.5 * t)]])
+        #self.v_cov = np.diag(self.Ncars * self.Ndim * [0.1])
+        self.u_func = lambda t: np.array([
+            [0, 0.1],
+            [-0.01, 0.1],
+            [0.03, 0.1]])
         #self.Nsecs = 10.0
         #self.Nsteps = int(self.Nsecs / self.dt)
         self.xs = np.zeros((1, self.Ncars, self.Ndim))
@@ -55,7 +59,9 @@ class ParticleFilter(object):
             num_particles=self.Np,
             num_cars=self.Ncars,
             num_state_dim=self.Ndim,
+            num_measurements=self.Nmeas,
             x0=self.x0,
+            init_cov=self.init_cov,
             x_cov=self.x_cov,
             measurement_cov=self.meas_cov,
             resample_perc=self.resample_perc)
@@ -67,7 +73,9 @@ class ParticleFilter(object):
         self.path_pub0 = rospy.Publisher("/path0", Path, queue_size=1)
         self.path_pub00 = rospy.Publisher("/path00", Path, queue_size=1)
         self.path_pub1 = rospy.Publisher("/path1", Path, queue_size=1)
+        self.path_pub11 = rospy.Publisher("/path11", Path, queue_size=1)
         self.path_pub2 = rospy.Publisher("/path2", Path, queue_size=1)
+        self.path_pub22 = rospy.Publisher("/path22", Path, queue_size=1)
 
     def run(self):
         start_time = rospy.get_time()
@@ -84,18 +92,27 @@ class ParticleFilter(object):
         path1.header = Header()
         path1.header.stamp = rospy.Time(0)
         path1.header.frame_id = "map"
+        path11 = Path()
+        path11.header = Header()
+        path11.header.stamp = rospy.Time(0)
+        path11.header.frame_id = "map"
         path2 = Path()
         path2.header = Header()
         path2.header.stamp = rospy.Time(0)
         path2.header.frame_id = "map"
+        path22 = Path()
+        path22.header = Header()
+        path22.header.stamp = rospy.Time(0)
+        path22.header.frame_id = "map"
 
         while not rospy.is_shutdown():
             self.current_time = rospy.get_time()
             dt = self.current_time - self.prev_time
-            #print dtdd
-
-            vs_actual = self.v_func(self.current_time - start_time)
-            self.xs = np.append(self.xs, [self.xs[i-1] + vs_actual * dt],axis=0)
+            
+            us = self.u_func(self.current_time - start_time)
+            self.xs = np.append(self.xs, [np.zeros((self.Ncars,self.Ndim))], axis=0)
+            for j in xrange(self.Ncars):
+                self.xs[i, j] = self.filter.state_transition(self.xs[i - 1, j], us[j], dt)
 
             pose0 = PoseStamped()
             pose0.header = Header()
@@ -122,22 +139,18 @@ class ParticleFilter(object):
             pose2.pose.orientation.w = 1
             path2.poses.append(pose2)
 
-            # vs = np.random.multivariate_normal(
-            #     vs_actual.flatten(), self.v_cov).reshape(self.Ncars, self.Ndim)
-            vs = vs_actual
-
             means = np.zeros((self.Ncars, self.Nmeas))
             for j in xrange(self.Ncars):
                 means[j, :2] = self.xs[i, j, :2]
+                means[j, 5] = self.xs[i, j, 3]
                 for k in xrange(self.Ncars):
                     if j != k:
-                        means[j, k + 2] = np.linalg.norm(self.xs[i, j] - self.xs[i, k])
+                        means[j, k + 2] = np.linalg.norm(self.xs[i, j, :2] - self.xs[i, k, :2])
             meas = np.random.multivariate_normal(
                 means.flatten(), self.meas_cov).reshape(self.Ncars, self.Nmeas)
-            self.filter.predict(vs, dt)
+            self.filter.update_particles(us, dt)
             self.filter.update_weights(meas)
             self.xs_pred = np.append(self.xs_pred, [self.filter.predicted_state()], axis=0)
-            print self.xs_pred[-1]
 
             pose00 = PoseStamped()
             pose00.header = Header()
@@ -147,14 +160,30 @@ class ParticleFilter(object):
             pose00.pose.position.y = self.xs_pred[i, 0, 1]
             pose00.pose.orientation.w = 1
             path00.poses.append(pose00)
+            pose11 = PoseStamped()
+            pose11.header = Header()
+            pose11.header.stamp = rospy.Time(0)
+            pose11.header.frame_id = "car1"
+            pose11.pose.position.x = self.xs_pred[i, 1, 0]
+            pose11.pose.position.y = self.xs_pred[i, 1, 1]
+            pose11.pose.orientation.w = 1
+            path11.poses.append(pose11)
+            pose22 = PoseStamped()
+            pose22.header = Header()
+            pose22.header.stamp = rospy.Time(0)
+            pose22.header.frame_id = "car2"
+            pose22.pose.position.x = self.xs_pred[i, 2, 0]
+            pose22.pose.position.y = self.xs_pred[i, 2, 1]
+            pose22.pose.orientation.w = 1
+            path22.poses.append(pose22)
 
-            if len(path00.poses) > 20:
-                path00.poses.pop(0)
+            # if len(path00.poses) > 20:
+            #     path00.poses.pop(0)
 
             self.filter.resample()
             self.error = np.append(self.error, np.zeros((1,)))
             for j in xrange(self.Ncars):
-                self.error[i] += np.linalg.norm(self.xs_pred[i, j] - self.xs[i, j]) / self.Ncars
+                self.error[i] += np.linalg.norm(self.xs_pred[i, j, :2] - self.xs[i, j, :2]) / self.Ncars
 
             self.prev_time = self.current_time
             i = i + 1
@@ -162,7 +191,9 @@ class ParticleFilter(object):
             self.path_pub0.publish(path0)
             self.path_pub00.publish(path00)
             self.path_pub1.publish(path1)
+            self.path_pub11.publish(path11)
             self.path_pub2.publish(path2)
+            self.path_pub22.publish(path22)
 
             self.rate.sleep()
 
--- src/particle_filter.py
+++ src/particle_filter.py
@@ -7,7 +7,7 @@ from scipy.stats import rv_discrete
 
 """
 State:
-    [x0, y0
+    [x0, y0, phi (angle), delta (steering angle), v
      x1, y1
      x2, y2]
 
@@ -17,9 +17,9 @@ Control:
      dx2, dy2]
 
 Measurements:
-    [gps_x0, gps_y0, uwb_00, uwb_01, uwb_02,
-     gps_x1, gps_y1, uwb_10, uwb_11, uwb_12
-     gps_x2, gps_y2, uwb_20, uwb_21, uwb_22]
+    [gps_x0, gps_y0, uwb_00, uwb_01, uwb_02, steerangle0, vel0
+     gps_x1, gps_y1, uwb_10, uwb_11, uwb_12, steerangle1, vel1
+     gps_x2, gps_y2, uwb_20, uwb_21, uwb_22, steerangle2, vel2]
 """
 
 
@@ -28,12 +28,15 @@ class MultiCarParticleFilter(object):
     def __init__(self, **kwargs):
         self.Np = kwargs.get("num_particles", 100)
         self.Ncars = kwargs.get("num_cars", 3)
-        self.Ndim = kwargs.get("num_state_dim", 2)
+        self.Ndim = kwargs.get("num_state_dim", 5)
         self.Nmeas = kwargs.get("num_measurements", 5)
         self.x0 = kwargs.get(
             "x0", np.array([[0, 0], [2, 1], [0, 1]]))
         self.x_cov = kwargs.get(
             "x_cov",
+            np.diag(self.Ncars * self.Ndim * [0.1]))
+        self.init_cov = kwargs.get(
+            "init_cov",
             np.diag(self.Ncars * self.Ndim * [0.1]))
         self.meas_cov = kwargs.get(
             "measurement_cov",
@@ -44,7 +47,7 @@ class MultiCarParticleFilter(object):
         self.resample_perc = kwargs.get("resample_perc", 0.3)
         self.particles = np.random.multivariate_normal(
             self.x0.flatten(),
-            self.x_cov,
+            self.init_cov,
             size=self.Np).reshape((self.Np, self.Ncars, self.Ndim))
         self.weights = 1.0 / self.Np * np.ones((self.Np,))
 
@@ -58,20 +61,24 @@ class MultiCarParticleFilter(object):
 
     def state_transition(self, x, u, dt):
         # u is a tuple (u_d, u_a)
-
-        k1 = self.state_transition_model(x, u)
-        k2 = self.state_transition_model(x + 0.5*dt*k1, u)
-        k3 = self.state_transition_model(x + 0.5*dt*k2, u)
-        k4 = self.state_transition_model(x + k3*dt, u)
-        new_x = x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
-        return new_x
+        steps = 1.
+        h = dt/steps
+        x = [x]
+        for i in range(0, int(steps)):
+            k1 = self.state_transition_model(x[i], u)
+            k2 = self.state_transition_model(x[i] + 0.5*h*k1, u)
+            k3 = self.state_transition_model(x[i] + 0.5*h*k2, u)
+            k4 = self.state_transition_model(x[i] + k3*h, u)
+            new_x = x[i] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+            x.append(new_x)
+        return x[-1]
 
     def state_transition_model(self, state, u):
         u_d, u_a, = u
         x, y, phi, delta, v = state
         dx = v*math.cos(phi)
         dy = v*math.sin(phi)
-        dphi = (v/3)*math.tan(delta)
+        dphi = (v/3.)*math.tan(delta)
         ddelta = u_d
         dv = u_a
         return np.array([dx, dy, dphi, ddelta, dv])
@@ -98,7 +105,11 @@ class MultiCarParticleFilter(object):
         for j in xrange(self.Np):
             p_means = np.zeros((self.Ncars, self.Nmeas))
             for k in xrange(self.Ncars):
+                # 'gps measurements'
                 p_means[k, :2] = self.particles[j, k, :2]
+                # p_means[k, 6] = self.particles[j, k, 4]
+                p_means[k, 5] = self.particles[j, k, 3]
+                # 'uwb measurements'
                 for l in xrange(self.Ncars):
                     if k != l:
                         p_means[k, l + 2] = np.linalg.norm(
@@ -127,26 +138,28 @@ class MultiCarParticleFilter(object):
 if __name__ == "__main__":
     from tqdm import trange
 
-    Np = 100
+    Np = 150
     Ncars = 3
     Ndim = 5
-    Nmeas = 5
+    Nmeas = 6
     dt = 0.1
-    x0 = np.array([[0, 0, 0, 0, 0],
-                   [2, 1, 0, 0, 0],
+    x0 = np.array([[0, 0, math.pi, 0, 0],
+                   [2, 1, math.pi/3., 0, 0],
                    [0, 1, 0, 0, 0]],
                   dtype=np.float64)
-    x_cov = np.diag(Ncars * [0.01, 0.01, 0, 0, 0])
-    measurement_cov = np.diag(Ncars * [0.1, 0.1, 0.01, 0.01, 0.01])
+    init_cov = np.diag(Ncars * [0.6, 0.6, 0.1, 0.00, 0.00])
+    x_cov = np.diag(Ncars * [0.1, 0.1, 0.1, 0.04, 0.0])
+    measurement_cov = np.diag(Ncars * [0.6, 0.6, 0.1, 0.1, 0.1, 0.01])
     actual_meas_cov = measurement_cov
     # actual_meas_cov = np.diag(Ncars * [0.001, 0.001, 0.01, 0.01, 0.01])
     u_func = lambda t: np.array([
         [0, 0.1],
         [-0.01, 0.1],
         [0.03, 0.1]])
-    Nsecs = 10.0
+    Nsecs = 20.0
     Nsteps = int(Nsecs / dt)
     xs = np.zeros((Nsteps, Ncars, Ndim))
+    measurements = np.zeros((Nsteps - 1, Ncars, Nmeas))
     xs_pred = np.zeros_like(xs)
     xs[0] = x0
     xs_pred[0] = x0
@@ -155,7 +168,9 @@ if __name__ == "__main__":
         num_particles=Np,
         num_cars=Ncars,
         num_state_dim=Ndim,
+        num_measurements=Nmeas,
         x0=x0,
+        init_cov=init_cov,
         x_cov=x_cov,
         measurement_cov=measurement_cov,
         resample_perc=0.3)
@@ -164,23 +179,34 @@ if __name__ == "__main__":
 
     for i in trange(1, Nsteps):
         us = u_func(i * dt)
+
         for j in xrange(Ncars):
             xs[i, j] = mcpf.state_transition(xs[i - 1, j], us[j], dt)
+            #xs[i] += mcpf.sample(np.zeros_like(x0), x_cov)
 
         means = np.zeros((Ncars, Nmeas))
+
         for j in xrange(Ncars):
+            # gps
             means[j, :2] = xs[i, j, :2]
+            # velocity
+            #means[j, 6] = xs[i, j, 4]
+            # steering angle
+            means[j, 5] = xs[i, j, 3]
+            # uwbs
             for k in xrange(Ncars):
                 if j != k:
-                    means[j, k + 2] = np.linalg.norm(xs[i, j] - xs[i, k])
+                    means[j, k + 2] = np.linalg.norm(xs[i, j, :2] - xs[i, k, :2])
         meas = np.random.multivariate_normal(
             means.flatten(), actual_meas_cov).reshape(Ncars, Nmeas)
+        measurements[i - 1] = meas
+
         mcpf.update_particles(us, dt)
         mcpf.update_weights(meas)
         xs_pred[i] = mcpf.predicted_state()
         mcpf.resample()
         for j in xrange(Ncars):
-            error[i] += np.linalg.norm(xs_pred[i, j] - xs[i, j]) / Ncars
+            error[i] += np.linalg.norm(xs_pred[i, j, :2] - xs[i, j, :2]) / Ncars
 
     fig, axes = plt.subplots(2, 1, figsize=(10, 8))
 
@@ -191,6 +217,10 @@ if __name__ == "__main__":
     axes[0].scatter(xs_pred[:, 0, 0], xs_pred[:, 0, 1], c="r")
     axes[0].scatter(xs_pred[:, 1, 0], xs_pred[:, 1, 1], c="g")
     axes[0].scatter(xs_pred[:, 2, 0], xs_pred[:, 2, 1], c="b")
+
+    axes[0].scatter(measurements[:, 0, 0], measurements[:, 0, 1], c="r", marker="*")
+    axes[0].scatter(measurements[:, 1, 0], measurements[:, 1, 1], c="g", marker="*")
+    axes[0].scatter(measurements[:, 2, 0], measurements[:, 2, 1], c="b", marker="*")
 
     axes[1].plot(error)
     plt.show()
