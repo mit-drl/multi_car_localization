b572bf1e74c2a0da6e127b942f8cb6b6a79b4804
Alexander Wallar <alex@wallar.me>
2017-09-02 21:49:45 -0400

    global frames?

--- src/initial_pose_node.py
+++ src/initial_pose_node.py
@@ -8,19 +8,23 @@ class InitialPoseNode(object):
 
     def __init__(self):
         self.num_cars = rospy.get_param("~num_cars", 3)
-        self.sub = rospy.Subscriber(
-            "/initialpose",
-            PoseWithCovarianceStamped,
-            self.initial_pose_cb, queue_size=1)
         self.pubs = list()
         for i in xrange(self.num_cars):
             topic = "/initial_pose_car{}".format(i)
             self.pubs.append(rospy.Publisher(
                 topic, PoseWithCovarianceStamped, queue_size=1))
+        self.sub = rospy.Subscriber(
+            "/initialpose",
+            PoseWithCovarianceStamped,
+            self.initial_pose_cb, queue_size=1)
         self.count = 0
+        self.rate = rospy.Rate(30)
 
     def initial_pose_cb(self, pwc):
-        self.pubs[self.count].publish(pwc)
+        rospy.loginfo("Publishing initpose to car{}".format(self.count % 3))
+        for i in xrange(50):
+            self.pubs[self.count % 3].publish(pwc)
+            self.rate.sleep()
         self.count += 1
 
 
