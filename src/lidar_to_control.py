4116b93b728376aea32503e887c37a26d89b0d77
Brandon A <br.araki@gmail.com>
2018-02-01 02:17:38 +0000

    bugs

--- src/lidar_to_control.py
+++ src/lidar_to_control.py
@@ -17,18 +17,20 @@ class LidarToControl(object):
 
 
     def lidar_cb(self, data):
-        if self.prev_lidar is None:
-            break
-        else:
+        if self.prev_lidar is not None:
             dt = (data.header.stamp - self.prev_lidar.header.stamp).to_sec()
             dx = data.pose.position.x - self.prev_lidar.pose.position.x
             dy = data.pose.position.y - self.prev_lidar.pose.position.y
             vel = (dx**2 + dy**2)**0.5 / dt
+            if dy < 0:
+            	vel = -1 * vel
 
             qp = self.prev_lidar.pose.orientation
+            qpp = (qp.x, qp.y, qp.z, qp.w)
             qd = data.pose.orientation
-            tp = euler_from_quaternion(qp)[2]
-            td = euler_from_quaternion(qd)[2]
+            qdd = (qd.x, qd.y, qd.z, qd.w)
+            tp = euler_from_quaternion(qpp)[2]
+            td = euler_from_quaternion(qdd)[2]
             dtheta = (td - tp)/dt
 
             control = CarControl()
@@ -38,6 +40,7 @@ class LidarToControl(object):
             control.velocity = vel
 
             self.control_pub.publish(control)
+	self.prev_lidar = data
 
 if __name__ == "__main__":
     rospy.init_node("lidar_to_control", anonymous=False)
