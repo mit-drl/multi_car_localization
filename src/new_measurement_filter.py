3f9f9abc46bf6ccbd7d17bbb776c43ce131c21d6
Brandon A <br.araki@gmail.com>
2018-02-19 22:54:36 -0500

    few more updates

--- src/new_measurement_filter.py
+++ src/new_measurement_filter.py
@@ -23,13 +23,13 @@ class NewParticleFilter(object):
 
     def __init__(self):
         self.rate = rospy.Rate(10)
-        self.Np = rospy.get_param("~num_particles", 3)
+        self.Np = rospy.get_param("~num_particles", 30)
         self.Ncars = rospy.get_param("/num_cars", 3)
         self.car_id = 1
         self.car_ids = [1, 2, 3]
         self.u_cov = np.matrix([0.1, 0.05, 0.1, 0.05, 0.1, 0.05]) # rospy.get_param("/u_cov", [0.15, 0.15, 0.05])
         self.uwb_cov = rospy.get_param("/uwb_cov", 0.05)
-        self.limits = np.matrix([0.5, 0.5, np.pi / 2, 0.5, 0.5, np.pi / 2]).T
+        self.limits = np.matrix([0.2, 0.2, np.pi / 2, 0.2, 0.2, np.pi / 2]).T
         self.bounds = np.asmatrix(np.zeros((3 * (self.Ncars - 1), 2)))
         self.circ_var = [0, 0, 1, 0, 0, 1]
 
@@ -41,14 +41,13 @@ class NewParticleFilter(object):
 
         self.control_sub = []
         self.controls_heap = []
-        self.car_controls = [[]] * self.Ncars
-        self.update_car_control = [True] * self.Ncars
+        self.car_controls = []
+        for i in range(self.Ncars):
+            self.car_controls.append([])
         self.max_control_times = [0]*3
-        self.prev_time = rospy.Time.now().to_sec()
+        self.prev_time = None
 
-        self.controls = np.matrix([0] * self.Ncars * 2).T
-        self.con_prev_time = [None] * self.Ncars # store prev times control received
-        self.con_prev_dt = [0.1] * self.Ncars # store dt's for each car's control msgs
+        self.controls = [0] * self.Ncars * 2
         self.range_sub = []
         self.pa_pub = rospy.Publisher("/car" + str(self.car_id) + "/particles",
                                       PoseArray, queue_size=1)
@@ -64,35 +63,42 @@ class NewParticleFilter(object):
 
     def range_cb(self, data, args):
         if self.initialized:
-            self.filter.correct(data.distance, data.from_id, data.to_id)
+            # self.filter.correct(data.distance, data.from_id-1, data.to_id-1)
+            return
 
     def control_cb(self, data, args):
         if self.initialized:
             index = args[0]
             data_time = data.header.stamp.to_sec()
             heapq.heappush(self.controls_heap, (data_time, data.car_id))
+
             self.max_control_times[index] = data_time
 
-            if not self.car_controls[index]:
-                self.controls[2*index, 0] = data.velocity
-                self.controls[2*index+1, 0] = data.steering_angle
-                self.update_car_control[index] = False
+            if len(self.car_controls[index]) == 0:
+                self.controls[2*index] = data.velocity
+                self.controls[2*index+1] = data.steering_angle
 
             self.car_controls[index].append(data)
 
+            # print self.controls_heap[0][0], min(self.max_control_times)
+
             while self.controls_heap[0][0] <= min(self.max_control_times):
                 control_info = heapq.heappop(self.controls_heap)
-                dt = control_info[0] - self.prev_time
+                if self.prev_time is None:
+                    dt = 0.1
+                else:
+                    dt = control_info[0] - self.prev_time
                 self.prev_time = control_info[0]
                 car_idx = control_info[1] - 1
                 old_control = self.car_controls[car_idx].pop(0)
 
-                self.filter.predict(dt, self.controls)
+                self.filter.predict(dt, np.asmatrix(self.controls).T)
+                # print self.controls
 
                 if self.car_controls[car_idx]:
-                    self.controls[2*car_idx, 0] = \
+                    self.controls[2*car_idx] = \
                         self.car_controls[car_idx][0].velocity
-                    self.controls[2*car_idx+1, 0] = \
+                    self.controls[2*car_idx+1] = \
                         self.car_controls[car_idx][0].steering_angle
 
     def set_bounds(self, transforms, limits):
@@ -111,11 +117,13 @@ class NewParticleFilter(object):
                     if i != self.car_id:
                         num = str(i)
                         now = rospy.Time(0)
-                        self.listener.waitForTransform("/vicon/car" + car_id + "/car" + car_id,
-                                                       "/vicon/car" + num + "/car" + num, now,
+                        starting_trans = "/vicon/car" + car_id + "/car" + car_id
+                        ending_trans = "/vicon/car" + num + "/car" + num
+                        self.listener.waitForTransform(starting_trans,
+                                                       ending_trans, now,
                                                        rospy.Duration(3.0))
-                        (trans, rot) = self.listener.lookupTransform("/vicon/car" + car_id + "/car" + car_id,
-                                                                     "/vicon/car" + num + "/car" + num, now)
+                        (trans, rot) = self.listener.lookupTransform(starting_trans,
+                                                                     ending_trans, now)
                         (r, p, y) = euler_from_quaternion(rot)
                         fi = 3*car_order_num
                         initial_transforms[fi:fi+2] = [[trans[0]], [trans[1]]]
@@ -133,7 +141,7 @@ class NewParticleFilter(object):
             else:
                 state = self.filter.get_state()
                 states = PoseArray()
-                states.header.stamp = rospy.Time.now()
+                states.header.stamp = rospy.Time(0)
                 states.header.frame_id = "/vicon/car1/car1"
                 for i in range(self.Ncars-1):
                     fi = 3*i
@@ -150,7 +158,7 @@ class NewParticleFilter(object):
 
                 particles = self.filter.particles
                 pa = PoseArray()
-                pa.header.stamp = rospy.Time.now()
+                pa.header.stamp = rospy.Time(0)
                 pa.header.frame_id = "/vicon/car1/car1"
                 for i in range(np.shape(particles)[0]):
                     particle = particles[i, :]
@@ -165,7 +173,6 @@ class NewParticleFilter(object):
                         p.orientation.z = q[2]
                         p.orientation.w = q[3]
                         pa.poses.append(p)
-                        print p
                 self.pa_pub.publish(pa)
 
 
